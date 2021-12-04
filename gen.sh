278c02099820e530741e8caad3c7475a1d9c636c
Daniel Armas <daniel.armas9706@gmail.com>
2021-12-04 15:20:48 -0500

    Modifyng the gen.sh. Generating the go code from the proto files.

--- gen.sh
+++ gen.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-protoc --proto_path=./protos protos/main.proto --plugin=$(go env GOPATH)/bin/protoc-gen-go-grpc --go-grpc_out=./lib/protos
-protoc --proto_path=./protos protos/main.proto --plugin=$(go env GOPATH)/bin/protoc-gen-go --go_out=./lib/protos
+protoc --proto_path=./protos protos/main.proto --plugin=$(go env GOPATH)/bin/protoc-gen-go-grpc --go-grpc_out=./src/protos
+protoc --proto_path=./protos protos/main.proto --plugin=$(go env GOPATH)/bin/protoc-gen-go --go_out=./src/protos
