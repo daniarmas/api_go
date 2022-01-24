72003f86be028831aa965d83460d33bf06ba44c7
Daniel Armas <daniel.armas9706@gmail.com>
2022-01-24 17:32:17 -0500

    Implementing the GetUser method in the usecase.

--- repository/user.go
+++ repository/user.go
@@ -7,21 +7,18 @@ import (
 
 type UserQuery interface {
 	GetUser(tx *gorm.DB, user *models.User, fields *[]string) (*models.User, error)
-	// ListItem() ([]models.Item, error)
 	CreateUser(tx *gorm.DB, user *models.User) (*models.User, error)
-	// UpdateItem(answer models.Item) (*models.Item, error)
-	// DeleteItem(id int64) error
 }
 
 type userQuery struct{}
 
-func (u *userQuery) GetUser(tx *gorm.DB, user *models.User, fields *[]string) (*models.User, error) {
+func (u *userQuery) GetUser(tx *gorm.DB, where *models.User, fields *[]string) (*models.User, error) {
 	var userResult *models.User
 	var result *gorm.DB
 	if fields != nil {
-		result = tx.Limit(1).Where(user).Select(*fields).Find(&userResult)
+		result = tx.Limit(1).Where(where).Select(*fields).Find(&userResult)
 	} else {
-		result = tx.Limit(1).Where(user).Find(&userResult)
+		result = tx.Limit(1).Where(where).Find(&userResult)
 	}
 	if result.Error != nil {
 		if result.Error.Error() == "record not found" {
---
+++ usecase/user.go
@@ -0,0 +1,58 @@
+package usecase
+
+import (
+	"errors"
+
+	"github.com/daniarmas/api_go/models"
+	"github.com/daniarmas/api_go/repository"
+	"github.com/google/uuid"
+	"google.golang.org/grpc/metadata"
+	"gorm.io/gorm"
+)
+
+type UserService interface {
+	GetUser(metadata *metadata.MD) (*models.User, error)
+}
+
+type userService struct {
+	dao repository.DAO
+}
+
+func NewUserService(dao repository.DAO) UserService {
+	return &userService{dao: dao}
+}
+
+func (i *userService) GetUser(metadata *metadata.MD) (*models.User, error) {
+	var user *models.User
+	var userErr error
+	err := repository.DB.Transaction(func(tx *gorm.DB) error {
+		authorizationTokenParseRes, authorizationTokenParseErr := i.dao.NewTokenQuery().ParseJwtAuthorizationToken(&metadata.Get("authorization")[0])
+		if authorizationTokenParseErr != nil {
+			switch authorizationTokenParseErr.Error() {
+			case "Token is expired":
+				return errors.New("authorizationtoken expired")
+			case "signature is invalid":
+				return errors.New("signature is invalid")
+			case "token contains an invalid number of segments":
+				return errors.New("token contains an invalid number of segments")
+			default:
+				return authorizationTokenParseErr
+			}
+		}
+		authorizationTokenRes, authorizationTokenErr := i.dao.NewAuthorizationTokenQuery().GetAuthorizationToken(tx, &models.AuthorizationToken{ID: uuid.MustParse(*authorizationTokenParseRes)}, &[]string{"id", "user_fk"})
+		if authorizationTokenErr != nil {
+			return authorizationTokenErr
+		} else if *authorizationTokenRes == (models.AuthorizationToken{}) {
+			return errors.New("unauthenticated")
+		}
+		user, userErr = i.dao.NewUserQuery().GetUser(tx, &models.User{ID: authorizationTokenRes.UserFk}, nil)
+		if userErr != nil {
+			return userErr
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
