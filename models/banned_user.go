ce5f2407484f241997502c56c7145f33441f3631
Daniel Armas <daniel.armas9706@gmail.com>
2022-05-13 09:34:38 -0400

    Implementing improves in the BannedUser model.

--- models/banned_user.go
+++ models/banned_user.go
@@ -14,17 +14,16 @@ func (BannedUser) TableName() string {
 }
 
 type BannedUser struct {
-	ID                            uuid.UUID          `gorm:"type:uuid;default:uuid_generate_v4()"`
-	Description                   string             `gorm:"column:description;not null"`
-	UserFk                        uuid.UUID          `gorm:"column:user_fk;not null"`
-	User                          User               `gorm:"foreignKey:UserFk"`
-	Email                         string             `gorm:"column:email;not null"`
-	ModeratorAuthorizationTokenFk uuid.UUID          `gorm:"column:moderator_authorization_token_fk;not null"`
-	AuthorizationToken            AuthorizationToken `gorm:"foreignKey:ModeratorAuthorizationTokenFk"`
-	BanExpirationTime             time.Time          `gorm:"column:ban_expiration_time;not null"`
-	CreateTime                    time.Time          `gorm:"column:create_time;not null"`
-	UpdateTime                    time.Time          `gorm:"column:update_time;not null"`
-	DeleteTime                    gorm.DeletedAt     `gorm:"index;column:delete_time"`
+	ID                uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4()"`
+	Description       string         `gorm:"column:description;not null"`
+	BanExpirationTime time.Time      `gorm:"column:ban_expiration_time;not null"`
+	UserId            uuid.UUID      `gorm:"column:user_id;not null"`
+	User              User           `gorm:"foreignKey:UserId"`
+	ModeratorId       uuid.UUID      `gorm:"column:moderator_id;not null"`
+	Moderator         User           `gorm:"foreignKey:ModeratorId"`
+	CreateTime        time.Time      `gorm:"column:create_time;not null"`
+	UpdateTime        time.Time      `gorm:"column:update_time;not null"`
+	DeleteTime        gorm.DeletedAt `gorm:"index;column:delete_time"`
 }
 
 func (i *BannedUser) BeforeCreate(tx *gorm.DB) (err error) {
