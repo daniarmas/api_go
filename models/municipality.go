cbe1b33660106452f776bd1666db9437600fe588
Daniel Armas <daniel.armas9706@gmail.com>
2022-05-13 09:51:28 -0400

    Improvint he Municipality and OrderLifeCycle models.

--- models/municipality.go
+++ models/municipality.go
@@ -21,6 +21,7 @@ type Municipality struct {
 	Coordinates ewkb.Point     `gorm:"column:coordinates"`
 	Polygon     ewkb.Polygon   `gorm:"column:polygon"`
 	ProvinceId  uuid.UUID      `gorm:"column:province_id;not null"`
+	Province    Province       `gorm:"foreignKey:ProvinceId"`
 	CreateTime  time.Time      `gorm:"column:create_time;not null"`
 	UpdateTime  time.Time      `gorm:"column:update_time;not null"`
 	DeleteTime  gorm.DeletedAt `gorm:"index;column:delete_time"`
