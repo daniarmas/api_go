87ab46b5b3cedfe394c8f25895da8409e83ed5bb
Daniel Armas <daniel.armas9706@gmail.com>
2022-05-13 09:15:46 -0400

    Implementing improves in the CartItem model.

--- models/cart_item.go
+++ models/cart_item.go
@@ -18,26 +18,13 @@ type CartItem struct {
 	Name                 string         `gorm:"column:name;not null"`
 	Price                float64        `gorm:"column:price;not null"`
 	Quantity             int32          `gorm:"column:quantity;not null"`
-	ItemFk               uuid.UUID      `gorm:"column:item_fk;not null"`
-	BusinessFk           uuid.UUID      `gorm:"column:business_fk;not null"`
-	Business             Business       `gorm:"foreignKey:BusinessFk"`
-	UserFk               uuid.UUID      `gorm:"column:user_fk;not null"`
-	AuthorizationTokenFk uuid.UUID      `gorm:"column:authorization_token_fk;not null"`
-	CreateTime           time.Time      `gorm:"column:create_time;not null"`
-	UpdateTime           time.Time      `gorm:"column:update_time;not null"`
-	DeleteTime           gorm.DeletedAt `gorm:"index;column:delete_time"`
-}
-
-type CartItemAndItem struct {
-	ID                   uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4()"`
-	Name                 string         `gorm:"column:name;not null"`
-	Price                float64        `gorm:"column:price;not null"`
-	Quantity             int32          `gorm:"column:quantity;not null"`
-	ItemFk               uuid.UUID      `gorm:"column:item_fk;not null"`
-	UserFk               uuid.UUID      `gorm:"column:user_fk;not null"`
-	AuthorizationTokenFk uuid.UUID      `gorm:"column:authorization_token_fk;not null"`
+	ItemId               uuid.UUID      `gorm:"column:item_id;not null"`
+	BusinessId           uuid.UUID      `gorm:"column:business_id;not null"`
+	Business             Business       `gorm:"foreignKey:BusinessId"`
 	Thumbnail            string         `gorm:"column:thumbnail;not null"`
 	ThumbnailBlurHash    string         `gorm:"column:thumbnail_blurhash;not null"`
+	UserId               uuid.UUID      `gorm:"column:user_id;not null"`
+	AuthorizationTokenId uuid.UUID      `gorm:"column:authorization_token_id;not null"`
 	CreateTime           time.Time      `gorm:"column:create_time;not null"`
 	UpdateTime           time.Time      `gorm:"column:update_time;not null"`
 	DeleteTime           gorm.DeletedAt `gorm:"index;column:delete_time"`
