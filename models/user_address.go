11a9be427df80879a1d78aa95ebc2d261b6b6460
Daniel Armas <daniel.armas9706@gmail.com>
2022-05-12 21:49:02 -0400

    Improving the UserAddress.

--- models/user_address.go
+++ models/user_address.go
@@ -17,14 +17,14 @@ func (UserAddress) TableName() string {
 type UserAddress struct {
 	ID             uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4()"`
 	Tag            string         `gorm:"column:tag;not null"`
-	UserFk         uuid.UUID      `gorm:"column:user_fk;not null"`
+	UserId         uuid.UUID      `gorm:"column:user_id;not null"`
 	Coordinates    ewkb.Point     `gorm:"column:coordinates;not null"`
 	ResidenceType  string         `gorm:"column:residence_type;not null"`
-	HouseNumber    string         `gorm:"column:house_number"`
-	BuildingNumber string         `gorm:"column:building_number"`
-	Description    string         `gorm:"column:description"`
-	ProvinceFk     uuid.UUID      `gorm:"column:province_fk;not null"`
-	MunicipalityFk uuid.UUID      `gorm:"column:municipality_fk;not null"`
+	Address        string         `gorm:"column:address;not null"`
+	Number         string         `gorm:"column:number;not null"`
+	Instructions   string         `gorm:"column:instructions"`
+	ProvinceId     uuid.UUID      `gorm:"column:province_id;not null"`
+	MunicipalityId uuid.UUID      `gorm:"column:municipality_id;not null"`
 	CreateTime     time.Time      `gorm:"column:create_time;not null"`
 	UpdateTime     time.Time      `gorm:"column:update_time;not null"`
 	DeleteTime     gorm.DeletedAt `gorm:"index;column:delete_time"`
