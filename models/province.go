df7e7a6ad17b4cec1c65ec85df8cd7addc648b49
Daniel Armas <daniel.armas9706@gmail.com>
2022-05-12 21:42:41 -0400

    Fixing the Province model.

--- models/province.go
+++ models/province.go
@@ -16,7 +16,7 @@ func (Province) TableName() string {
 type Province struct {
 	ID                       uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4()"`
 	Name                     string         `gorm:"column:name;not null"`
-	ProvinceNameAbbreviation string         `gorm:"column:abbreviation_province_name;not null"`
+	Codename                 string         `gorm:"column:codename;not null"`
 	CreateTime               time.Time      `gorm:"column:create_time;not null"`
 	UpdateTime               time.Time      `gorm:"column:update_time;not null"`
 	DeleteTime               gorm.DeletedAt `gorm:"index;column:delete_time"`
