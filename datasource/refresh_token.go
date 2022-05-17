aa1cad7b9e23d3424288831f5ad5576996b1bed3
Daniel Armas <daniel.armas9706@gmail.com>
2022-05-17 11:38:03 -0400

    Improving the RefreshToken datasource.

--- datasource/refresh_token.go
+++ datasource/refresh_token.go
@@ -1,50 +1,55 @@
 package datasource
 
 import (
+	"errors"
+
 	"github.com/daniarmas/api_go/models"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
 
 type RefreshTokenDatasource interface {
-	GetRefreshToken(tx *gorm.DB, refreshToken *models.RefreshToken, fields *[]string) (*models.RefreshToken, error)
-	CreateRefreshToken(tx *gorm.DB, refreshToken *models.RefreshToken) (*models.RefreshToken, error)
-	DeleteRefreshToken(tx *gorm.DB, refreshToken *models.RefreshToken, fields *[]string) (*[]models.RefreshToken, error)
+	GetRefreshToken(tx *gorm.DB, where *models.RefreshToken) (*models.RefreshToken, error)
+	CreateRefreshToken(tx *gorm.DB, data *models.RefreshToken) (*models.RefreshToken, error)
+	DeleteRefreshToken(tx *gorm.DB, where *models.RefreshToken, ids []uuid.UUID) (*[]models.RefreshToken, error)
 }
 
 type refreshTokenDatasource struct{}
 
-func (v *refreshTokenDatasource) CreateRefreshToken(tx *gorm.DB, refreshToken *models.RefreshToken) (*models.RefreshToken, error) {
-	result := tx.Create(&refreshToken)
+func (v *refreshTokenDatasource) CreateRefreshToken(tx *gorm.DB, data *models.RefreshToken) (*models.RefreshToken, error) {
+	result := tx.Create(&data)
 	if result.Error != nil {
 		return nil, result.Error
 	}
-	return refreshToken, nil
+	return data, nil
 }
 
-func (r *refreshTokenDatasource) DeleteRefreshToken(tx *gorm.DB, refreshToken *models.RefreshToken, fields *[]string) (*[]models.RefreshToken, error) {
-	var refreshTokenResultSlice *[]models.RefreshToken
-	result := tx.Clauses(clause.Returning{}).Where(refreshToken).Delete(&refreshTokenResultSlice)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-	return refreshTokenResultSlice, nil
-}
-
-func (r *refreshTokenDatasource) GetRefreshToken(tx *gorm.DB, refreshToken *models.RefreshToken, fields *[]string) (*models.RefreshToken, error) {
-	var refreshTokenResult *models.RefreshToken
+func (r *refreshTokenDatasource) DeleteRefreshToken(tx *gorm.DB, where *models.RefreshToken, ids []uuid.UUID) (*[]models.RefreshToken, error) {
+	var res *[]models.RefreshToken
 	var result *gorm.DB
-	if fields != nil {
-		result = tx.Limit(1).Where(refreshToken).Select(*fields).Find(&refreshTokenResult)
+	if len(ids) != 0 {
+		result = tx.Clauses(clause.Returning{}).Where(`id IN ?`, ids).Delete(&res)
 	} else {
-		result = tx.Limit(1).Where(refreshToken).Find(&refreshTokenResult)
+		result = tx.Clauses(clause.Returning{}).Where(where).Delete(&res)
 	}
+	if result.Error != nil {
+		return nil, result.Error
+	} else if result.RowsAffected == 0 {
+		return nil, errors.New("record not found")
+	}
+	return res, nil
+}
+
+func (v *refreshTokenDatasource) GetRefreshToken(tx *gorm.DB, where *models.RefreshToken) (*models.RefreshToken, error) {
+	var res *models.RefreshToken
+	result := tx.Where(where).Take(&res)
 	if result.Error != nil {
 		if result.Error.Error() == "record not found" {
-			return refreshTokenResult, nil
+			return nil, errors.New("refreshtoken not found")
 		} else {
 			return nil, result.Error
 		}
 	}
-	return refreshTokenResult, nil
+	return res, nil
 }
