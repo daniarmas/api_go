7058e98244d2abe0809d743adb4e279ec5b30813
Daniel Armas <daniel.armas9706@gmail.com>
2022-06-05 20:21:14 -0400

    Implementing improvements in the business_collection datasource.

--- datasource/business_collection.go
+++ datasource/business_collection.go
@@ -8,24 +8,32 @@ import (
 )
 
 type BusinessCollectionDatasource interface {
-	GetBusinessCollection(tx *gorm.DB, where *models.BusinessCollection) (*models.BusinessCollection, error)
-	ListBusinessCollection(tx *gorm.DB, where *models.BusinessCollection) (*[]models.BusinessCollection, error)
+	GetBusinessCollection(tx *gorm.DB, where *models.BusinessCollection, fields *[]string) (*models.BusinessCollection, error)
+	ListBusinessCollection(tx *gorm.DB, where *models.BusinessCollection, fields *[]string) (*[]models.BusinessCollection, error)
 }
 
 type businessCollectionDatasource struct{}
 
-func (i *businessCollectionDatasource) ListBusinessCollection(tx *gorm.DB, where *models.BusinessCollection) (*[]models.BusinessCollection, error) {
+func (i *businessCollectionDatasource) ListBusinessCollection(tx *gorm.DB, where *models.BusinessCollection, fields *[]string) (*[]models.BusinessCollection, error) {
 	var itemsCategory []models.BusinessCollection
-	result := tx.Where(where).Find(&itemsCategory)
+	selectFields := &[]string{"*"}
+	if fields == nil {
+		selectFields = fields
+	}
+	result := tx.Where(where).Select(*selectFields).Find(&itemsCategory)
 	if result.Error != nil {
 		return nil, result.Error
 	}
 	return &itemsCategory, nil
 }
 
-func (i *businessCollectionDatasource) GetBusinessCollection(tx *gorm.DB, where *models.BusinessCollection) (*models.BusinessCollection, error) {
+func (i *businessCollectionDatasource) GetBusinessCollection(tx *gorm.DB, where *models.BusinessCollection, fields *[]string) (*models.BusinessCollection, error) {
 	var businessBusinessCollection *models.BusinessCollection
-	result := tx.Where(where).Take(&businessBusinessCollection)
+	selectFields := &[]string{"*"}
+	if fields == nil {
+		selectFields = fields
+	}
+	result := tx.Where(where).Select(*selectFields).Take(&businessBusinessCollection)
 	if result.Error != nil {
 		if result.Error.Error() == "record not found" {
 			return nil, errors.New("record not found")
