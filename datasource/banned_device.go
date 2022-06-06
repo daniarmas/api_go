bc872ca16f18a66178a4efea7a9bf8618b4a130d
Daniel Armas <daniel.armas9706@gmail.com>
2022-06-05 20:19:03 -0400

    Implementing improvements in the BannedDevice datasource.

--- datasource/banned_device.go
+++ datasource/banned_device.go
@@ -15,7 +15,11 @@ type bannedDeviceDatasource struct{}
 
 func (v *bannedDeviceDatasource) GetBannedDevice(tx *gorm.DB, where *models.BannedDevice, fields *[]string) (*models.BannedDevice, error) {
 	var res *models.BannedDevice
-	result := tx.Where(where).Select(*fields).Take(&res)
+	selectFields := &[]string{"*"}
+	if fields == nil {
+		selectFields = fields
+	}
+	result := tx.Where(where).Select(*selectFields).Take(&res)
 	if result.Error != nil {
 		if result.Error.Error() == "record not found" {
 			return nil, errors.New("record not found")
